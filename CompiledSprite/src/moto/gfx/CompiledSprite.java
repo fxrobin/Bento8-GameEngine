97b18022511cc75b75215d81d3356660310ea7d8
a453953 <a453953@FR-MP1ENC8R.ww930.my-it-solutions.net>
2019-11-13 12:20:31 +0100

    ajout commentaire sur boot dans compiledsprite.java

--- CompiledSprite/src/moto/gfx/CompiledSprite.java
+++ CompiledSprite/src/moto/gfx/CompiledSprite.java
@@ -55,6 +55,37 @@ public class CompiledSprite
 			Files.write(fichier, sprite.getCodeDataPos("E2"), Charset.forName("UTF-8"), StandardOpenOption.APPEND);	
 			
 			Files.write(fichier, sprite.getCodePalette(background.getColorModel(),2), Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+			// BOOTLOADER 6300
+//			***************************************
+//			* Boot loader. Il charge le 2eme
+//			* secteur de la diskette de boot en
+//			* $6300 et saute a cette adresse.
+//			***************************************
+//			(main)bootld.ASS
+//			   setdp $60
+//			   org $6200
+//
+//			init
+//			   lda #$02
+//			   sta <$6048 * DK.OPC $02 Operation - Lecture d un secteur
+//			   sta <$604C * DK.SEC $02 Secteur a lire
+//			   ldd $1E    * 
+//			   std ,s     * 
+//			   ldd #$6300 * Destination des donnees lues
+//			   std <$604F * DK.BUF Destination des donnees lues
+//			   jsr $E82A  * DKFORM Appel 
+//			   stb <$6080 * Semaphore du controle de presence du controleur de disque
+//			   bcs exit   * Si Erreur C=1 alors branchement exit
+//			   jmp $6300  * Sinon Execution en $6300
+//			exit
+//			   rts        * Retour au programme residant
+//			   end init			
+			
+			// <bh:86><bh:02><bh:97>H<bh:97>L<bh:fc><bh:00><bh:1e><bh:ed><bh:e4><bh:cc>c<bh:00><bh:dd>O<bh:bd><bh:e8>*<bh:d7><bh:80>%<bh:03>~c<bh:00>9
+			// Ecrire les val hex ci dessus en debut de fichier (limite 0-120) avec la formule : 256-(val dec) ex: 86=7A
+			// En position octet 121 : "BASIC2"
+			// En position octet 126 : $00
+			// En position octet 127 : s=85 pour chaque octet : =MOD(s-(valeur lue);256)
 		}
 		else {
 			System.out.println("Parametres invalides !");
