bea767f13bf22afb2a5a74bc0e2b02b04e15bb4d
A453953 <A453953@DESKTOP-SGBUNGS>
2020-10-23 16:29:22 +0200

    Ajout ObjectMove et ObjectMoveAndFall OK

--- CompiledSprite/input/ENGINE/AnimateSprite.asm
+++ CompiledSprite/input/ENGINE/AnimateSprite.asm
@@ -128,40 +128,51 @@ Anim_End                               *Anim_End:
                                        *; End of function AnimateSprite
                                        
 (include)Constant
+MainCharacter                 equ $0000
+Sidekick                      equ $0000
+
+* ---------------------------------------------------------------------------
+* Physics Constants
+* ---------------------------------------------------------------------------
+
+gravity                       equ $38 ; Gravité: 56 sub-pixels par frame
+
 * ---------------------------------------------------------------------------
 * Object Status Table offsets
 * ---------------------------------------------------------------------------
 
-object_size                   equ $20 ; the size of an object
+object_size                   equ $1F ; the size of an object
 next_object                   equ object_size
 			                  
 id                            equ $00 ; object ID (00: free slot, 01: Object1, ...)
 render_flags                  equ $01 ; bitfield
-x_pos                         equ $02 ; and $03 ... some objects use $A and $B as well when extra precision is required (see ObjectMove)
-x_pixel                       equ $04 ;
-x_sub                         equ $05 ; and $06 ; subpixel
-y_pos                         equ $07 ; and $08 ... some objects use $E and $F as well when extra precision is required
-y_pixel                       equ $09 ;
-y_sub                         equ $0A ; and $0B ; subpixel
-priority                      equ $0C ; 0 equ front
-width_pixels                  equ $0D
-x_vel                         equ $0E ; and $0F ; horizontal velocity
-y_vel                         equ $10 ; and $11 ; vertical velocity
-y_radius                      equ $12 ; collision height / 2
-x_radius                      equ $13 ; collision width / 2
-anim_frame                    equ $14
-anim                          equ $15
-prev_anim                     equ $16
-anim_frame_duration           equ $17
-status                        equ $18 ; note: exact meaning depends on the object...
-routine                       equ $19
-routine_secondary             equ $1A
-objoff_01                     equ $1B ; variables génériques
-objoff_02                     equ $1C
-objoff_03                     equ $1D
-objoff_04                     equ $1E
-objoff_05                     equ $1F
-collision_flags               equ $20
+x_pos                         equ $02 ; and $03 ... some objects use subpixel as well when extra precision is required (see ObjectMove)
+x_sub                         equ $04 ; subpixel ; doit suivre x_pos, second octet supprimé car inutile en 6809
+y_pos                         equ $05 ; and $06 ... some objects use subpixel as well when extra precision is required
+y_sub                         equ $07 ; subpixel ; doit suivre y_pos, second octet supprimé car inutile en 6809
+x_pixel                       equ x_pos
+y_pixel                       equ x_pos+2
+priority                      equ $08 ; 0 equ front
+width_pixels                  equ $09
+mapping_frame                 equ $0A
+x_vel                         equ $0B ; and $0C ; horizontal velocity
+y_vel                         equ $0D ; and $0E ; vertical velocity
+y_radius                      equ $0F ; collision height / 2
+x_radius                      equ $10 ; collision width / 2
+anim_frame                    equ $11
+anim                          equ $12
+prev_anim                     equ $13
+anim_frame_duration           equ $14 ; range: 00-7F (0-127)
+status                        equ $15 ; note: exact meaning depends on the object...
+routine                       equ $16
+routine_secondary             equ $17
+objoff_01                     equ $18 ; variables spécifiques aux objets
+objoff_02                     equ $19
+objoff_03                     equ $1A
+objoff_04                     equ $1B
+objoff_05                     equ $1C
+collision_flags               equ $1D
+subtype                       equ $1E
 
 * ---------------------------------------------------------------------------
 * render_flags bitfield variables
--- CompiledSprite/input/SPRITE/Shellcracker/Shellcracker.asm
+++ CompiledSprite/input/SPRITE/Shellcracker/Shellcracker.asm
@@ -15,9 +15,9 @@ ObjID_ShellcrackerClaw equ $03
 * ---------------------------------------------------------------------------
 * Object Status Table offsets
 * ---------------------------------------------------------------------------
-delay    equ $1C ; and $1D (nombre de frames+1)
-parent   equ $1E ; and $1F (adresse OST de l'objet parent)
-instance equ $20 (numéro d'instance du sous objet, w dans code 68k, b dans le code 6809)
+delay    equ $1A ; and $1B (nombre de frames+1)
+parent   equ $1C ; and $1D (adresse OST de l'objet parent)
+instance equ $1E (numéro d'instance du sous objet, w dans code 68k, b dans le code 6809)
 
                                        *; ===========================================================================
                                        *; ----------------------------------------------------------------------------
@@ -415,7 +415,10 @@ ShellcrackerClaw_Retract_02            *loc_3827A:
                                        *; ===========================================================================
                                        *
 ShellcrackerClaw_Proj                  *loc_38280:
-        jsr   ObjectMoveAndFall        *    jsrto   (ObjectMoveAndFall).l, JmpTo8_ObjectMoveAndFall
+        jsr   ObjectMove               *    jsrto   (ObjectMoveAndFall).l, JmpTo8_ObjectMoveAndFall
+        ldd   y_vel,x                  ; ce code complète ObjectMove pour faire l'équivalent de ObjectMoveAndFall
+        addd  gravity                  ; ...
+        std   y_vel,x                  ; fin
         ldd   delay,x
         subd  #$0001
         std   delay,x                  *    subq.w  #1,objoff_2A(a0)
@@ -502,42 +505,51 @@ Ani_Shellcracker_Walk fcb $0E,$00,$01,$02,$FF
                                        *; ===========================================================================           
                                        
 (include)Constant
+MainCharacter                 equ $0000
+Sidekick                      equ $0000
+
+* ---------------------------------------------------------------------------
+* Physics Constants
+* ---------------------------------------------------------------------------
+
+gravity                       equ $38 ; Gravité: 56 sub-pixels par frame
+
 * ---------------------------------------------------------------------------
 * Object Status Table offsets
 * ---------------------------------------------------------------------------
 
-object_size                   equ $22 ; the size of an object
+object_size                   equ $1F ; the size of an object
 next_object                   equ object_size
 			                  
 id                            equ $00 ; object ID (00: free slot, 01: Object1, ...)
 render_flags                  equ $01 ; bitfield
-x_pos                         equ $02 ; and $03 ... some objects use $A and $B as well when extra precision is required (see ObjectMove)
-x_pixel                       equ $04 ;
-x_sub                         equ $05 ; and $06 ; subpixel
-y_pos                         equ $07 ; and $08 ... some objects use $E and $F as well when extra precision is required
-y_pixel                       equ $09 ;
-y_sub                         equ $0A ; and $0B ; subpixel
-priority                      equ $0C ; 0 equ front
-width_pixels                  equ $0D
-mapping_frame                 equ $0E
-x_vel                         equ $0F ; and $10 ; horizontal velocity
-y_vel                         equ $11 ; and $12 ; vertical velocity
-y_radius                      equ $13 ; collision height / 2
-x_radius                      equ $14 ; collision width / 2
-anim_frame                    equ $15
-anim                          equ $16
-prev_anim                     equ $17
-anim_frame_duration           equ $18 ; range: 00-7F (0-127)
-status                        equ $19 ; note: exact meaning depends on the object...
-routine                       equ $1A
-routine_secondary             equ $1B
-objoff_01                     equ $1C ; variables spécifiques aux objets
-objoff_02                     equ $1D
-objoff_03                     equ $1E
-objoff_04                     equ $1F
-objoff_05                     equ $20
-collision_flags               equ $21
-subtype                       equ $22
+x_pos                         equ $02 ; and $03 ... some objects use subpixel as well when extra precision is required (see ObjectMove)
+x_sub                         equ $04 ; subpixel ; doit suivre x_pos, second octet supprimé car inutile en 6809
+y_pos                         equ $05 ; and $06 ... some objects use subpixel as well when extra precision is required
+y_sub                         equ $07 ; subpixel ; doit suivre y_pos, second octet supprimé car inutile en 6809
+x_pixel                       equ x_pos
+y_pixel                       equ x_pos+2
+priority                      equ $08 ; 0 equ front
+width_pixels                  equ $09
+mapping_frame                 equ $0A
+x_vel                         equ $0B ; and $0C ; horizontal velocity
+y_vel                         equ $0D ; and $0E ; vertical velocity
+y_radius                      equ $0F ; collision height / 2
+x_radius                      equ $10 ; collision width / 2
+anim_frame                    equ $11
+anim                          equ $12
+prev_anim                     equ $13
+anim_frame_duration           equ $14 ; range: 00-7F (0-127)
+status                        equ $15 ; note: exact meaning depends on the object...
+routine                       equ $16
+routine_secondary             equ $17
+objoff_01                     equ $18 ; variables spécifiques aux objets
+objoff_02                     equ $19
+objoff_03                     equ $1A
+objoff_04                     equ $1B
+objoff_05                     equ $1C
+collision_flags               equ $1D
+subtype                       equ $1E
 
 * ---------------------------------------------------------------------------
 * render_flags bitfield variables
