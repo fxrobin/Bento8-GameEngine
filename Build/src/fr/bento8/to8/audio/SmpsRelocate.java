4f37e854b84e038265f2ab262091c4844a5000a2
A453953 <A453953@DESKTOP-SGBUNGS>
2021-07-04 00:06:44 +0200

    Voice Map auto from YM2612 to YM2413

--- Build/src/fr/bento8/to8/audio/SmpsRelocate.java
+++ Build/src/fr/bento8/to8/audio/SmpsRelocate.java
@@ -4,30 +4,36 @@ import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+import fr.bento8.to8.audio.YmVoice.OPNVoice;
+import fr.bento8.to8.audio.YmVoice.OPNVoice.OPNSlotParam;
 
 public class SmpsRelocate{
-	
+
 	private static int SMPS_VOICE = 0;
 	private static int SMPS_NB_FM = 2;
 	private static int SMPS_NB_PSG = 3;
 	private static int SMPS_FM_START = 6;
 	private static int SMPS_FM_SIZE = 4;
 	private static int SMPS_PSG_SIZE = 6;
-	
+
+	private static byte[] voiceMap = new byte[256];
 
 	private static byte[] fIN;
 	private static FileOutputStream fOUT;
 	private static int offset = 0; //4992 pour S2
-	
+
 	public static void main(String[] args) throws Throwable {
 
-		System.out.println("*** Relocate Smps pointers ***");
+		System.out.println("*** YM2612 to YM2413 Smps converter ***");
 
-		if (args.length != 2) {
-			System.out.println("Arguments: inputfile address_offset");
+		if (args.length !=2 && args.length != 3) {
+			System.out.println("Arguments: inputfile address_offset voicefile");
 			System.out.println(" the first parameter is the smps file.");
 			System.out.println(" the second parameter is the address offset to apply to pointers in the smps file.");
 			System.out.println(" ex: -10 will substract 10 to address pointers");
+			System.out.println(" the third parameter is the voice mapping file (optional)");
 			return;
 		}
 
@@ -36,72 +42,94 @@ public class SmpsRelocate{
 			System.out.println("Fatal: can't open input file");
 			return;
 		}
-		
+
 		offset = Integer.parseInt(args[1]);
-		
+
 		fOUT = new FileOutputStream(args[1]);
 		if (fOUT == null) {
 			System.out.println("Fatal: can't write to output PSG file");
 			return;
 		}
-		
+
 		// Relocate Voice
 		// ********************************************************************
-		
-		relocate(SMPS_VOICE);
-		
+
+		int voicePos = relocate(SMPS_VOICE);
+
+		// Load Voices
+		if (args.length == 3) {
+			Path voiceFile = Path.of(args[2]);
+			if (voiceFile == null) {
+				System.out.println("Fatal: can't input voice file");
+				return;			
+			}
+			String voice = Files.readString(voiceFile);
+			StringTokenizer voices = new StringTokenizer(voice, " .,;-/:|");
+			int vi = 0;
+			while (voices.hasMoreTokens())
+			{
+				voiceMap[vi++] = hexStringToByteArray(voices.nextToken()+"0")[0];	
+			}
+		} else {
+			int vi = 0, curVoice = voicePos;
+			while (curVoice<fIN.length) {
+				voiceMap[vi++] = (byte)(estimateOPPLLVoice1234(curVoice) << 4);
+				curVoice += 25;
+			}
+		}		
+
 		// Relocate Tracks
 		// ********************************************************************		
-		
+
 		int nbFMTracks = fIN[SMPS_NB_FM];
 		int pos = SMPS_FM_START;
 		for (int i = 0; i < nbFMTracks; i++) {
 			relocateTrack(pos);
 			pos += SMPS_FM_SIZE;
 		}
-		
+
 		int nbPSGTracks = fIN[SMPS_NB_PSG];
 		for (int i = 0; i < nbPSGTracks; i++) {
 			relocateTrack(pos);
 			pos += SMPS_PSG_SIZE;
 		}		
-		
+
 		while (pos < fIN.length) {
 			// Coordination flags
 			// ********************************************************************
 			switch (fIN[pos]) {
-				case (byte)0xE6: //E6xx - volume
-					fIN[pos+1] = (byte) (fIN[pos+1] / 8);
-			    	pos += 2;
-			    	break;				
-				case (byte)0xEF: //EFxx - voice
-					fIN[pos+1] = (byte) (fIN[pos+1] << 4);
-			    	pos += 2;
-			    	break;				    	
-				case (byte)0xF0: //F0wwxxyyzz - modulation TODO piste FM seulement !!!
-					modulation(pos+2);
-			    	pos += 5;
-			    	break;				
-				case (byte)0xF6: //$F6zzzz
-				case (byte)0xF8: //$F8zzzz					
-					relocateOffsetBack(pos+1);
-				    pos += 3;
-					break;
-				case (byte)0xF7: //$F7xxyyzzzz
-					relocateOffsetBack(pos+3);
-				    pos += 5;
-					break;				
-				default:
-					pos++;
-					break;
+			case (byte)0xE6: //E6xx - volume
+				fIN[pos+1] = (byte) (fIN[pos+1] / 8);
+			pos += 2;
+			break;				
+			case (byte)0xEF: //EFxx - voice
+				fIN[pos+1] = voiceMap[fIN[pos+1]];
+			pos += 2;
+			break;				    	
+			case (byte)0xF0: //F0wwxxyyzz - modulation TODO piste FM seulement !!!
+				modulation(pos+2);
+			pos += 5;
+			break;				
+			case (byte)0xF6: //$F6zzzz
+			case (byte)0xF8: //$F8zzzz					
+				relocateOffsetBack(pos+1);
+			pos += 3;
+			break;
+			case (byte)0xF7: //$F7xxyyzzzz
+				relocateOffsetBack(pos+3);
+			pos += 5;
+			break;				
+			default:
+				pos++;
+				break;
 			}			
-			
+
 		}				
 		Path path = Paths.get(args[0]+".smp");
 		Files.write(path, fIN);
 	}
-	
-	private static void relocate (int pos) throws Exception {
+
+	private static int relocate (int pos) throws Exception {
 		if (pos > fIN.length) {
 			throw new Exception ("File is invalid.");
 		}
@@ -109,6 +137,7 @@ public class SmpsRelocate{
 		address += offset;
 		fIN[pos] = (byte) (address >> 8);
 		fIN[pos+1] = (byte) (address);
+		return address;
 	}
 
 	private static void relocateTrack (int pos) throws Exception {
@@ -121,7 +150,7 @@ public class SmpsRelocate{
 		fIN[pos+1] = (byte) (address);
 		fIN[pos+3] = (byte) (fIN[pos+3] / 8); // Volume
 	}
-	
+
 	private static void relocateOffsetBack (int pos) throws Exception {
 		if (pos > fIN.length) {
 			throw new Exception ("File is invalid.");
@@ -138,5 +167,44 @@ public class SmpsRelocate{
 		}
 		int value = (fIN[pos] & 0xff);
 		fIN[pos] = (byte) (Math.ceil(value/3.733333333));
+	}	
+
+	/* s must be an even-length string. */
+	private static byte[] hexStringToByteArray(String s) {
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+					+ Character.digit(s.charAt(i+1), 16));
+		}
+		return data;
+	}	
+
+	public static byte estimateOPPLLVoice1234 (int pos) {
+		OPNVoice opn = new OPNVoice(
+				(byte)((fIN[pos] & 0x38) >> 3),
+				(byte)(fIN[pos] & 0x07),
+				(byte)0,
+				(byte)0,
+				new OPNSlotParam((byte)((fIN[pos+1+0] & 0x70) >> 4), (byte)(fIN[pos+1+0] & 0xF), (byte)(fIN[pos+21+0] & 0x7F), (byte)((fIN[pos+5+0] & 0x70) >> 6), (byte)(fIN[pos+5+0] & 0x1F), (byte)((fIN[pos+9+0] & 0x80) >> 7), (byte)(fIN[pos+9+0] & 0x7F), (byte)(fIN[pos+13+0] & 0x1F), (byte)((fIN[pos+17+0] & 0xF0) >> 4), (byte)(fIN[pos+17+0] & 0xF), (byte)0),
+				new OPNSlotParam((byte)((fIN[pos+1+1] & 0x70) >> 4), (byte)(fIN[pos+1+1] & 0xF), (byte)(fIN[pos+21+1] & 0x7F), (byte)((fIN[pos+5+1] & 0x70) >> 6), (byte)(fIN[pos+5+1] & 0x1F), (byte)((fIN[pos+9+1] & 0x80) >> 7), (byte)(fIN[pos+9+1] & 0x7F), (byte)(fIN[pos+13+1] & 0x1F), (byte)((fIN[pos+17+1] & 0xF0) >> 4), (byte)(fIN[pos+17+1] & 0xF), (byte)0),
+				new OPNSlotParam((byte)((fIN[pos+1+2] & 0x70) >> 4), (byte)(fIN[pos+1+2] & 0xF), (byte)(fIN[pos+21+2] & 0x7F), (byte)((fIN[pos+5+2] & 0x70) >> 6), (byte)(fIN[pos+5+2] & 0x1F), (byte)((fIN[pos+9+2] & 0x80) >> 7), (byte)(fIN[pos+9+2] & 0x7F), (byte)(fIN[pos+13+2] & 0x1F), (byte)((fIN[pos+17+2] & 0xF0) >> 4), (byte)(fIN[pos+17+2] & 0xF), (byte)0),
+				new OPNSlotParam((byte)((fIN[pos+1+3] & 0x70) >> 4), (byte)(fIN[pos+1+3] & 0xF), (byte)(fIN[pos+21+3] & 0x7F), (byte)((fIN[pos+5+3] & 0x70) >> 6), (byte)(fIN[pos+5+3] & 0x1F), (byte)((fIN[pos+9+3] & 0x80) >> 7), (byte)(fIN[pos+9+3] & 0x7F), (byte)(fIN[pos+13+3] & 0x1F), (byte)((fIN[pos+17+3] & 0xF0) >> 4), (byte)(fIN[pos+17+3] & 0xF), (byte)0)
+				);
+		return opn.toOPL().toOPLLROMVoice()[0];
+	}
+	
+	public static byte estimateOPPLLVoice1324 (int pos) {
+		OPNVoice opn = new OPNVoice(
+				(byte)((fIN[pos] & 0x38) >> 3),
+				(byte)(fIN[pos] & 0x07),
+				(byte)0,
+				(byte)0,
+				new OPNSlotParam((byte)((fIN[pos+1+0] & 0x70) >> 4), (byte)(fIN[pos+1+0] & 0xF), (byte)(fIN[pos+21+0] & 0x7F), (byte)((fIN[pos+5+0] & 0x70) >> 6), (byte)(fIN[pos+5+0] & 0x1F), (byte)((fIN[pos+9+0] & 0x80) >> 7), (byte)(fIN[pos+9+0] & 0x7F), (byte)(fIN[pos+13+0] & 0x1F), (byte)((fIN[pos+17+0] & 0xF0) >> 4), (byte)(fIN[pos+17+0] & 0xF), (byte)0),
+				new OPNSlotParam((byte)((fIN[pos+1+2] & 0x70) >> 4), (byte)(fIN[pos+1+2] & 0xF), (byte)(fIN[pos+21+2] & 0x7F), (byte)((fIN[pos+5+2] & 0x70) >> 6), (byte)(fIN[pos+5+2] & 0x1F), (byte)((fIN[pos+9+2] & 0x80) >> 7), (byte)(fIN[pos+9+2] & 0x7F), (byte)(fIN[pos+13+2] & 0x1F), (byte)((fIN[pos+17+2] & 0xF0) >> 4), (byte)(fIN[pos+17+2] & 0xF), (byte)0),
+				new OPNSlotParam((byte)((fIN[pos+1+1] & 0x70) >> 4), (byte)(fIN[pos+1+1] & 0xF), (byte)(fIN[pos+21+1] & 0x7F), (byte)((fIN[pos+5+1] & 0x70) >> 6), (byte)(fIN[pos+5+1] & 0x1F), (byte)((fIN[pos+9+1] & 0x80) >> 7), (byte)(fIN[pos+9+1] & 0x7F), (byte)(fIN[pos+13+1] & 0x1F), (byte)((fIN[pos+17+1] & 0xF0) >> 4), (byte)(fIN[pos+17+1] & 0xF), (byte)0),				
+				new OPNSlotParam((byte)((fIN[pos+1+3] & 0x70) >> 4), (byte)(fIN[pos+1+3] & 0xF), (byte)(fIN[pos+21+3] & 0x7F), (byte)((fIN[pos+5+3] & 0x70) >> 6), (byte)(fIN[pos+5+3] & 0x1F), (byte)((fIN[pos+9+3] & 0x80) >> 7), (byte)(fIN[pos+9+3] & 0x7F), (byte)(fIN[pos+13+3] & 0x1F), (byte)((fIN[pos+17+3] & 0xF0) >> 4), (byte)(fIN[pos+17+3] & 0xF), (byte)0)
+				);
+		return opn.toOPL().toOPLLROMVoice()[0];
 	}	
 }
---
+++ Build/src/fr/bento8/to8/audio/YmVoice/OPNVoice.java
@@ -0,0 +1,144 @@
+package fr.bento8.to8.audio.YmVoice;
+
+import fr.bento8.to8.audio.YmVoice.OPLVoice.OPLSlotParam;
+
+public class OPNVoice {
+
+	public byte fb, con, ams, pms;
+	public OPNSlotParam[] slots;
+
+	public static class OPNSlotParam {
+		byte dt;
+		byte ml;
+		byte tl;
+		byte ks;
+		byte ar;
+		byte am;
+		byte dr;
+		byte sr;
+		byte sl;
+		byte rr;
+		byte ssg;
+
+		public OPNSlotParam(OPNSlotParam init) {
+			this.dt = init.dt;
+			this.ml = init.ml;
+			this.tl = init.tl;
+			this.ks = init.ks;
+			this.ar = init.ar;
+			this.am = init.am;
+			this.dr = init.dr;
+			this.sr = init.sr;
+			this.sl = init.sl;
+			this.rr = init.rr;
+			this.ssg = init.ssg;
+		};
+
+		public OPNSlotParam(byte dt, byte ml, byte tl, byte ks, byte ar, byte am, byte dr, byte sr, byte sl, byte rr, byte ssg) {
+			this.dt = dt;
+			this.ml = ml;
+			this.tl = tl;
+			this.ks = ks;
+			this.ar = ar;
+			this.am = am;
+			this.dr = dr;
+			this.sr = sr;
+			this.sl = sl;
+			this.rr = rr;
+			this.ssg = ssg;
+		};	
+
+		private OPLSlotParam toOPLSlotParam() {
+			OPLSlotParam sp = new OPLSlotParam(
+					am,
+					(byte)0,
+					(byte)0,
+					(byte)(ks >> 1),
+					ml,
+					(byte)0,
+					(byte)Math.min(63, tl),
+					_AR(ar),
+					_DR(dr),
+					sl,
+					_DR(sr),
+					(byte)0
+					);
+			return sp;
+		}	
+
+		private byte _AR(byte a) {
+			switch (a) {
+			case 31:
+				return 15;
+			case 0:
+				return 0;
+			default:
+				return (byte) Math.max(1, Math.min(15, (a * 28) >> 6));
+			}
+		}
+		private byte _DR(byte a) {
+			switch (a) {
+			case 31:
+				return 15;
+			case 0:
+				return 0;
+			default:
+				return (byte) Math.max(1, Math.min(15, (a * 28) >> 6));
+			}
+		}
+	}  	
+
+	public OPNVoice(byte fb, byte con, byte ams, byte pms, OPNSlotParam s0, OPNSlotParam s1, OPNSlotParam s2, OPNSlotParam s3) {
+		this.fb = fb;
+		this.con = con;
+		this.ams = ams;
+		this.pms = pms;
+		this.slots = new OPNSlotParam[] {s0, s1, s2, s3};		
+	}
+
+	public OPNVoice(byte fb, byte con, byte ams, byte pms, OPNSlotParam[] slots) {
+		this.fb = fb;
+		this.con = con;
+		this.ams = ams;
+		this.pms = pms;
+		this.slots = new OPNSlotParam[] {new OPNSlotParam(slots[0]), new OPNSlotParam(slots[1]), new OPNSlotParam(slots[2]), new OPNSlotParam(slots[3])};		
+	}	
+
+	public OPLVoice toOPL() {
+		OPLSlotParam[] ss = new OPLSlotParam[] {
+				this.slots[0].toOPLSlotParam(),
+				this.slots[1].toOPLSlotParam(),
+				this.slots[2].toOPLSlotParam(),
+				this.slots[3].toOPLSlotParam()
+		};
+
+		OPLSlotParam s0, s1;
+		switch (this.con) {
+		case 0: case 3:
+			s0 = ss[0];
+			s1 = ss[3];
+			s1.setMl(ss[1].ml);
+			s1.setTl((byte)(Math.min(63, Math.max(0, ss[1].tl - 2) + ss[3].tl)));
+			return new OPLVoice(this.fb, (byte)0, new OPLSlotParam[] {s0, s1});
+		case 1:
+			s0 = ss[0];
+			s1 = ss[3];
+			s1.setMl(ss[2].ml);
+			s1.setTl((byte)(Math.min(63, Math.max(0, ss[2].tl - 2) + ss[3].tl)));
+			return new OPLVoice(this.fb, (byte)0, new OPLSlotParam[] {s0, s1});	    	  
+		case 2:
+			s0 = ss[0];
+			s1 = ss[3];
+			return new OPLVoice(this.fb, (byte)0, new OPLSlotParam[] {s0, s1});	    	  
+		case 4: case 5: case 6:
+			s0 = ss[0];
+			s1 = ss[1];
+			return new OPLVoice(this.fb, (byte)0, new OPLSlotParam[] {s0, s1});	    
+		default:
+			s0 = ss[0];
+			s1 = ss[1];
+			return new OPLVoice(this.fb, (byte)1, new OPLSlotParam[] {s0, s1});	  
+		}
+	}
+
+}
